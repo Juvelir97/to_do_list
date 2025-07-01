943eb29645f4bfe83657092ab898739fe7ff40bb
Oleg Ivanov <juvelir97@protonmail.com>
2025-07-01 17:21:54 +0300

    Added models

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from models import TodoItem
 
 app = FastAPI()
+
+todo_list = []
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to my To-Do list"}
+
+@app.get("/todos")
+def show_todos():
+    return {"message": "your todos list", "list" : todo_list}
+@app.post('/todos')
+def create_todo(item: TodoItem):
+    todo_list.append(item)
+    return {'message': 'todo item created','item': item}
---
+++ models.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from datetime import datetime
+from enum import Enum
+from pydantic import BaseModel,model_validator
+
+class StatusEnum(str, Enum):
+    pending = "pending"
+    in_progress = "in_progress"
+    done = "done"
+
+class TodoItem(BaseModel):
+    title: str
+    status: StatusEnum
+    start_date: datetime
+    end_date: Optional[datetime] = None
+    description: Optional[str] = None
+
+    @model_validator(mode='after')
+    def check_dates(self):
+        if self.start_date < self.end_date:
+            raise ValueError('end_date must be after start_date')
+        return self
+
